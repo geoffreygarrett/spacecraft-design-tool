b02d26d8c47c74d503ddb912dd81d18be4ec2f69
Geoffrey <g.h.garrett13@gmail.com>
2018-04-01 02:58:18 +0800

    Authorship

---
+++ tools.py
@@ -0,0 +1,9 @@
+# Authorship -----------------------------------------------------------------------------#
+__author__      = ["Geoffrey Hyde Garrett", "Vladimir"]
+__copyright__   = None
+__credits__     = None
+__license__     = "MIT"
+__version__     = "1.0.0"
+__maintainer__  = ["Geoffrey Hyde Garrett", "Vladimir"]
+__email__       = ["g.h.garrett13@gmail.com", "vladifm97@gmail.com"]
+__status__      = "Development"
